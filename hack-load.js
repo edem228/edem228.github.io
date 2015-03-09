3832b2fef7a37180f27982e5af92bfdb71e83df3
edem228 <edem228@gmail.com>
2015-03-09 00:42:20 +0000

    Signed-off-by: edem228 <edem228@gmail.com>

---
+++ hack-load.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('.mk-jupiter-icon-simple-wordpress').attr('title', 'viadeo');
+    $('.widget_social_networks a[title="Suivez nous sur wordpress"]').attr('title', 'Suivez nous sur viadeo');
+});
